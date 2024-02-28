97916194bd4106cdca81f3ee5babd635b443262d
Giovanni León Corujo <giovannilc.dev@gmail.com>
2024-02-28 19:31:49 +0000

    Formateo de todos los documentos con Prittier

--- src/templates/Página principal/script.js
+++ src/templates/Página principal/script.js
@@ -1,40 +1,35 @@
-document.addEventListener('DOMContentLoaded', async function() {
-    await cargarEstructura();
-    await cargarEstilo();
+document.addEventListener("DOMContentLoaded", async function () {
+	await cargarEstructura();
+	await cargarEstilo();
 });
 
-
 async function cargarEstructura() {
-    let appBody = document.querySelector("body");
-    await cargarComponente(appBody, '../../componentes/Cabecera/');
-    await cargarComponente(appBody, '../../componentes/Horario/');
-    await cargarComponente(appBody, '../../componentes/Footer/');
+	let appBody = document.querySelector("body");
+	await cargarComponente(appBody, "../../componentes/Cabecera/");
+	await cargarComponente(appBody, "../../componentes/Horario/");
+	await cargarComponente(appBody, "../../componentes/Footer/");
 }
-
-
 
 async function cargarComponente(body, url) {
-    document.head.appendChild(await cargarEstilo(url));
-    body.appendChild(await cargarTemplate(url))
+	document.head.appendChild(await cargarEstilo(url));
+	body.appendChild(await cargarTemplate(url));
 }
-
-
 
 async function cargarTemplate(url) {
-    let response = await fetch(url+"index.html");
-    let text = await response.text();
+	let response = await fetch(url + "index.html");
+	let text = await response.text();
 
-    let template = document.createElement('template');
-    template.innerHTML = text;
+	let template = document.createElement("template");
+	template.innerHTML = text;
 
-    return document.importNode(template.content, true);
+	return document.importNode(template.content, true);
 }
 
-async function cargarEstilo(url){
-    var linkElement = document.createElement("link");
+async function cargarEstilo(url) {
+	var linkElement = document.createElement("link");
 
-    linkElement.href = url+"style.css";
-    linkElement.rel = "stylesheet";
+	linkElement.href = url + "style.css";
+	linkElement.rel = "stylesheet";
 
-    return linkElement;
+	return linkElement;
 }
