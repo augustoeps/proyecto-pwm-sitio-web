52cd9909f749ab461d6dec3acd93857244f10d63
Giovanni León Corujo <giovannilc.dev@gmail.com>
2024-03-01 00:05:13 +0000

    Creada página de servicios

---
+++ src/templates/Página de servicios/script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", async function () {
+	await cargarEstructura();
+});
+
+async function cargarEstructura() {
+	let appBody = document.querySelector("body");
+	await cargarComponente(appBody, "../../componentes/Cabecera/");
+	await cargarComponente(appBody, "../../componentes/Servicios/");
+	await cargarComponente(appBody, "../../componentes/Footer/");
+}
+
+async function cargarComponente(body, url) {
+	document.head.appendChild(await cargarEstilo(url));
+	body.appendChild(await cargarTemplate(url));
+}
+
+async function cargarTemplate(url) {
+	let response = await fetch(url + "index.html");
+	let text = await response.text();
+
+	let template = document.createElement("template");
+	template.innerHTML = text;
+
+	return document.importNode(template.content, true);
+}
+
+async function cargarEstilo(url) {
+	var linkElement = document.createElement("link");
+
+	linkElement.href = url + "style.css";
+	linkElement.rel = "stylesheet";
+
+	return linkElement;
+}
