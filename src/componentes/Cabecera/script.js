96a7adf7aa8ecf11331bae839103fd5cd391f0a8
Giovanni León Corujo <giovannilc.dev@gmail.com>
2024-03-13 18:33:38 +0000

    cabecera totalmente responsive finalizada

--- src/componentes/Cabecera/script.js
+++ src/componentes/Cabecera/script.js
@@ -13,7 +13,7 @@ function header_animation_show(header){
     header.classList.add("header-animated-left")
     header.disabled = true
     header.addEventListener("animationend", () => {
-        header.style.transform = "translateX(-210px)";
+        header.style.transform = "translateX(-100em)";
         setTimeout(function() {
             header_button.disabled = false
             hided = false
@@ -26,7 +26,7 @@ function header_animation_hide(header){
     header.classList.add("header-animated-right")
     header.disabled = true
     header.addEventListener("animationend", () => {
-        header.style.transform = "translateX(200px)"; 
+        header.style.transform = "translateX(100em)"; 
         setTimeout(function() {
             header_button.disabled = false
             hided = true
