e02af55409fb6529aa39bfb7b72c4b085309639e
Giovanni <giovannilc.dev@gmail.com>
2024-04-15 11:05:09 +0100

    rutas relativas a assets modificadas a absolutas

--- src/app/main-page/components/schedule-info/DataLoader.ts
+++ src/app/main-page/components/schedule-info/DataLoader.ts
@@ -2,7 +2,7 @@
 export class DataLoader{
 	private async loaddata() {
 		const response = await fetch(
-			'../../../../assets/jsons/InfoTaller.json'
+			'/assets/jsons/InfoTaller.json'
 		);
 		const json = await response.json();
 		const InfoTaller = await json.data;
