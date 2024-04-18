c6f080b9342c867aa5635adc96d1ac5100cf81b3
augusto <augustopaolone3@hotmail.com>
2024-04-18 11:17:41 +0200

    verificacion de correo y contraseña

--- src/app/auth-module/auth-module.module.ts
+++ src/app/auth-module/auth-module.module.ts
@@ -4,9 +4,19 @@ import { LogInComponent } from './components/log-in/log-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { DataLoader } from '../services/DataLoader';
 import { GlobalModule } from '../global-module/global-module.module';
+import {FormsModule} from "@angular/forms";
+import {AngularFireModule} from "@angular/fire/compat";
 
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDyQfa18rE-wFiMwaHU-Ht1FhhiULdqlw8",
+  authDomain: "test-5dfd9.firebaseapp.com",
+  projectId: "test-5dfd9",
+  storageBucket: "test-5dfd9.appspot.com",
+  messagingSenderId: "580473688063",
+  appId: "1:580473688063:web:3695a221ed569403c4e1ca",
+  measurementId: "G-5RZ5QJRMK0"
+};
 
 @NgModule({
   declarations: [
@@ -16,7 +26,9 @@ import { GlobalModule } from '../global-module/global-module.module';
   providers: [DataLoader],
   imports: [
     CommonModule,
-    GlobalModule
+    GlobalModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [SignUpComponent]
 })
