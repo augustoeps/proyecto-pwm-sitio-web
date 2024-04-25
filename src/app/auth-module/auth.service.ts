594533e3d85960b03787e073b27bb6e58703d43d
Giovanni <giovannilc.dev@gmail.com>
2024-04-25 10:01:36 +0100

    arreglado  responsive de la página de servicios, reseñas y footer

--- src/app/auth-module/auth.service.ts
+++ src/app/auth-module/auth.service.ts
@@ -1,57 +1,57 @@
 import { Injectable } from '@angular/core';
-import {AngularFireAuth} from "@angular/fire/compat/auth";
+import { AngularFireAuth } from "@angular/fire/compat/auth";
 import firebase from "firebase/compat";
-import {AngularFirestore} from "@angular/fire/compat/firestore";
+import { AngularFirestore } from "@angular/fire/compat/firestore";
 import { filter, map, Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private afauth: AngularFireAuth, private firestore: AngularFirestore) { }
+    constructor(private afauth: AngularFireAuth, private firestore: AngularFirestore) { }
 
 
-  async register(email: string, password: string, name: string) {
-    try {
-      const result = await this.afauth.createUserWithEmailAndPassword(email, password);
+    async register(email: string, password: string, name: string) {
+        try {
+            const result = await this.afauth.createUserWithEmailAndPassword(email, password);
 
-      if (result && result.user) {
+            if (result && result.user) {
 
-        await this.firestore.collection('users').doc(result.user.uid).set({
-          email: email,
-          name: name,
-          photourl:"",
-          uid:result.user.uid
-        });
-        return result;
-      } else {
-        throw new Error('No se pudo crear el usuario.');
-      }
-    } catch (error) {
-      console.error('Error en el registro:', error);
-      return null;
+                await this.firestore.collection('users').doc(result.user.uid).set({
+                    email: email,
+                    name: name,
+                    photourl: "",
+                    uid: result.user.uid
+                });
+                return result;
+            } else {
+                throw new Error('No se pudo crear el usuario.');
+            }
+        } catch (error) {
+            console.error('Error en el registro:', error);
+            return null;
+        }
     }
-  }
 
-  async login(email:string,password:string){
+    async login(email: string, password: string) {
 
-        return await this.afauth.signInWithEmailAndPassword(email,password)
+        return await this.afauth.signInWithEmailAndPassword(email, password)
 
-  }
+    }
 
-  obtenerUser(){
+    obtenerUser() {
 
-    return this.afauth.authState
+        return this.afauth.authState
 
-  }
-  obtenerDatosUsuario(uid: string): Observable<any> {
-    return this.firestore.collection('users').doc(uid).valueChanges();
-  }
+    }
+    obtenerDatosUsuario(uid: string): Observable<any> {
+        return this.firestore.collection('users').doc(uid).valueChanges();
+    }
 
-  logout() {
-    return this.afauth.signOut();
-  }
+    logout() {
+        return this.afauth.signOut();
+    }
 
 
 
