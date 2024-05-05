7756472c81c96e5ddf12e120ac985776810eb208
Giovanni León Corujo <giovannilc.dev@gmail.com>
2024-05-05 12:39:36 +0100

    Todas la imagenes en el servidor

--- src/app/global-module/components/header/header.component.ts
+++ src/app/global-module/components/header/header.component.ts
@@ -1,133 +1,131 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
 import { AuthService } from "../../../auth-module/auth.service"
 import { AngularFireStorage } from "@angular/fire/compat/storage";
-import { of, switchMap } from "rxjs";
-import { user } from "@angular/fire/auth";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
 
-  @ViewChild('header_button') header_button!: ElementRef;
-  @ViewChild('header') header!: ElementRef;
-  hided: boolean;
-  animationClass: string;
-  email: string | null = ""
-  nombre: string | null = ""
-  image: string = "/assets/services/perfil.webp"
-  @Input() mostrarLogin: boolean = true;
-  selectedFile: File | null = null;
-  uid: string | null = ""
-  imagePreviewUrl = ""
-  constructor(private authService: AuthService, private storage: AngularFireStorage) {
-    this.hided = true;
-    this.animationClass = 'none';
-  }
+    @ViewChild('header_button') header_button!: ElementRef;
+    @ViewChild('header') header!: ElementRef;
+    hided: boolean;
+    animationClass: string;
+    email: string | null = ""
+    nombre: string | null = ""
+    image: string = "https://firebasestorage.googleapis.com/v0/b/test-5dfd9.appspot.com/o/assets%2FPortrait_Placeholder.webp?alt=media&token=906bc66e-828c-43bd-aa3c-59b85e87a16d"
+    @Input() mostrarLogin: boolean = true;
+    selectedFile: File | null = null;
+    uid: string | null = ""
+    imagePreviewUrl = ""
+    constructor(private authService: AuthService, private storage: AngularFireStorage) {
+        this.hided = true;
+        this.animationClass = 'none';
+    }
 
-  ngOnInit(): void {
-    this.authService.obtenerUser()
-    this.authService.obtenerUser().subscribe(user => {
-      if (user) {
-        this.email = user.email
-        this.authService.obtenerDatosUsuario(user.uid).subscribe(data => {
-          this.nombre = data.name
-          if (!data.firtstime) {
-            this.image = data.photoURL
-          }
+    ngOnInit(): void {
+        this.authService.obtenerUser()
+        this.authService.obtenerUser().subscribe(user => {
+            if (user) {
+                this.email = user.email
+                this.authService.obtenerDatosUsuario(user.uid).subscribe(data => {
+                    this.nombre = data.name
+                    if (!data.firtstime) {
+                        this.image = data.photoURL
+                    }
+
+                })
+                if (typeof this.image !== 'undefined') {
+
+                }
+            } else {
+                this.email = ""
+            }
 
         })
-        if (typeof this.image !== 'undefined') {
-          console.log("No hay foto")
+    }
+
+
+
+    test() {
+        if (this.email == "") {
+
+            return true
+        } else {
+
+            return false
         }
-      } else {
-        this.email = ""
-      }
-
-    })
-  }
-
-
-
-  test() {
-    if (this.email == "") {
-
-      return true
-    } else {
-
-      return false
-    }
-  }
-
-
-  onFileSelected(event: any): void {
-    this.selectedFile = event.target.files?.[0] || null;
-    if (this.selectedFile) {
-      this.imagePreviewUrl = URL.createObjectURL(this.selectedFile);
     }
 
-    this.uploadProfilePhoto()
-  }
 
-
-  async uploadProfilePhoto(): Promise<void> {
-    if (this.selectedFile) {
-
-      const user = await this.authService.obtenerUser();
-
-      if (user) {
-        try {
-
-
-          const uid = this.authService.getCurrentUserUid();
-          this.uid = await uid
-          await this.authService.actualizarValorBooleano(this.uid || "ff");
-          const photoURL = await this.authService.updateProfilePhoto(this.uid || "ff", this.selectedFile);
-
-          console.log('URL de la imagen de perfil actualizada:', photoURL);
-
-
-        } catch (error) {
-          console.error('Error al subir la imagen de perfil:', error);
+    onFileSelected(event: any): void {
+        this.selectedFile = event.target.files?.[0] || null;
+        if (this.selectedFile) {
+            this.imagePreviewUrl = URL.createObjectURL(this.selectedFile);
         }
-      } else {
-        console.error('Usuario no autenticado.');
-      }
-    } else {
-      console.error('No se seleccionó ningún archivo.');
+
+        this.uploadProfilePhoto()
     }
-  }
 
 
+    async uploadProfilePhoto(): Promise<void> {
+        if (this.selectedFile) {
+
+            const user = await this.authService.obtenerUser();
+
+            if (user) {
+                try {
 
 
+                    const uid = this.authService.getCurrentUserUid();
+                    this.uid = await uid
+                    await this.authService.actualizarValorBooleano(this.uid || "ff");
+                    const photoURL = await this.authService.updateProfilePhoto(this.uid || "ff", this.selectedFile);
 
-  logout() {
+                    console.log('URL de la imagen de perfil actualizada:', photoURL);
 
-    this.authService.logout()
-  }
 
-  changeHeaderVisibility(): void {
-    if (this.animationClass != 'header-animated-left') {
-      this.header_animation_show(this.header.nativeElement);
-    } else {
-      this.header_animation_hide(this.header.nativeElement);
+                } catch (error) {
+                    console.error('Error al subir la imagen de perfil:', error);
+                }
+            } else {
+                console.error('Usuario no autenticado.');
+            }
+        } else {
+            console.error('No se seleccionó ningún archivo.');
+        }
     }
-  }
 
-  private header_animation_show(header: any): void {
-    this.animationClass = 'header-animated-left';
-    header.addEventListener('animationend', () => {
-      header.style.transform = 'translateX(-100em)';
-    });
-  }
 
-  private header_animation_hide(header: any): void {
-    this.animationClass = 'header-animated-right';
-    header.addEventListener('animationend', () => {
-      header.style.transform = 'translateX(100em)';
-    });
-  }
+
+
+
+    logout() {
+
+        this.authService.logout()
+    }
+
+    changeHeaderVisibility(): void {
+        if (this.animationClass != 'header-animated-left') {
+            this.header_animation_show(this.header.nativeElement);
+        } else {
+            this.header_animation_hide(this.header.nativeElement);
+        }
+    }
+
+    private header_animation_show(header: any): void {
+        this.animationClass = 'header-animated-left';
+        header.addEventListener('animationend', () => {
+            header.style.transform = 'translateX(-100em)';
+        });
+    }
+
+    private header_animation_hide(header: any): void {
+        this.animationClass = 'header-animated-right';
+        header.addEventListener('animationend', () => {
+            header.style.transform = 'translateX(100em)';
+        });
+    }
 }
--- src/app/service-module/components/services-loader/card-service/card-service.component.ts
+++ src/app/service-module/components/services-loader/card-service/card-service.component.ts
@@ -1,13 +1,17 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-card-service',
-  templateUrl: './card-service.component.html',
-  styleUrl: './card-service.component.css'
+    selector: 'app-card-service',
+    templateUrl: './card-service.component.html',
+    styleUrl: './card-service.component.css'
 })
 export class CardServiceComponent {
 
-  @Input() Titulo! : String;
-  @Input() Precio! : String;
-  @Input() Imagen! : String;
+    @Input() Titulo!: String;
+    @Input() Precio!: String;
+    @Input() Imagen!: String
+
+    constructor() {
+        console.log(this.Imagen)
+    }
 }
