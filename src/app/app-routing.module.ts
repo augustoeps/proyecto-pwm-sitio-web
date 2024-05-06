88f82751b47947ffe456249e5fd7b0d90343dddb
Giovanni <giovannilc.dev@gmail.com>
2024-05-06 11:31:32 +0100

    initial ionic app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { MainPageComponent } from './home-module/components/main-page/main-page.component';
-import { ServicesComponent } from './service-module/components/services-loader/services.component';
-import {LogInComponent} from "./auth-module/components/log-in/log-in.component";
-import {SignUpComponent} from "./auth-module/components/sign-up/sign-up.component";
-import { ContactComponent } from './contact-module/contact-module.component';
-import {ReviewsComponent} from "./review-module/components/reviews/reviews.component";
-import { ServiceComponent } from "./service-module/components/service/service.component";
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:"", redirectTo:"Inicio", pathMatch:"full"},
-  {path:"Inicio", component:MainPageComponent},
-  {path:"Servicios", component:ServicesComponent},
-  {path:"login",component:LogInComponent},
-  {path:"signup",component:SignUpComponent},
-  {path:"Contactar",component:ContactComponent},
-  {path:"Reviews",component:ReviewsComponent},
-  {path:"Service/:id",component:ServiceComponent}
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
 import { AppComponent } from './app.component';
-import { RouterOutlet } from '@angular/router';
-import { HomeModule } from './home-module/home-module.module.module';
-import { ServiceModule } from './service-module/service-module.module';
-import { AuthModuleModule } from './auth-module/auth-module.module';
-import { DataLoader } from './services/DataLoader.service';
-import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
-import { ContactModule } from './contact-module/contact-module.module';
-import {ReviewModuleModule} from "./review-module/review-module.module";
-import { AngularFireModule } from "@angular/fire/compat";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDyQfa18rE-wFiMwaHU-Ht1FhhiULdqlw8",
-  authDomain: "test-5dfd9.firebaseapp.com",
-  projectId: "test-5dfd9",
-  storageBucket: "test-5dfd9.appspot.com",
-  messagingSenderId: "580473688063",
-  appId: "1:580473688063:web:3695a221ed569403c4e1ca",
-  databaseURL: "https://test-5dfd9-default-rtdb.europe-west1.firebasedatabase.app",
-  measurementId: "G-5RZ5QJRMK0"
-};
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule,
-    AppRoutingModule,
-    RouterOutlet,
-    HomeModule,
-    ServiceModule,
-    AuthModuleModule,
-    ContactModule,
-    HttpClientModule,
-    AngularFireModule.initializeApp(firebaseConfig),
-    ReviewModuleModule],
-  providers: [DataLoader, HttpClient],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
